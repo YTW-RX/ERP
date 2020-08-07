cbdec23846f3817826b7f8a6e77573ce847ae8a9
dan987528096 <987528096@qq.com>
2020-08-07 10:28:58 +0800

    2020 08-07 9:20  丹俊通提交

---
+++ src/main/java/com/zking/erp/services/baseManage/IGoodsTypeService.java
@@ -0,0 +1,11 @@
+package com.zking.erp.services.baseManage;
+
+import com.zking.erp.util.JsonResponseBody;
+
+public interface IGoodsTypeService {
+    /**
+     * 查询所有的商品类型列表
+     * @return
+     */
+    JsonResponseBody getAllGoodsType();
+}
